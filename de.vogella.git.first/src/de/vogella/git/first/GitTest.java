b6bdfab7fd142b94cc3567d937c7acd059defc62
EngMego <ahmed.abdelmajied@gmail.com>
2012-11-16 13:14:00 +0200

    face branch

--- de.vogella.git.first/src/de/vogella/git/first/GitTest.java
+++ de.vogella.git.first/src/de/vogella/git/first/GitTest.java
@@ -5,7 +5,7 @@ public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("Git is cool :D");
 
-		System.out.println("first after delete :P");
+		System.out.println("first after delete");
 	}
 
 }
