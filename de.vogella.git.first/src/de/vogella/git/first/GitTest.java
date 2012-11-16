ace124883f58832576e693f8249a24820a574ee8
EngMego <ahmed.abdelmajied@gmail.com>
2012-11-16 11:08:49 +0200

    change output massage

--- de.vogella.git.first/src/de/vogella/git/first/GitTest.java
+++ de.vogella.git.first/src/de/vogella/git/first/GitTest.java
@@ -3,7 +3,7 @@ package de.vogella.git.first;
 public class GitTest {
 
 	public static void main(String[] args) {
-		System.out.println("Git is fun :P");
+		System.out.println("Git is cool :D");
 
 	}
 
